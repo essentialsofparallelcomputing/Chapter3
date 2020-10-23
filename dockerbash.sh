da366c49d3cdb707219be3768a4f2562a0d22030
Bob Robey <brobey@lanl.gov>
2020-10-22 20:03:15 -0600

    Fixing containers to display graphics

--- dockerbash.sh
+++ dockerbash.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-docker build -f Dockerfile.Ubuntu20.04 -t essentialsofparallelcomputing/chapter3 .
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-#chmod 755 $XAUTH
 docker run -it --gpus all -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH --entrypoint /bin/bash essentialsofparallelcomputing/chapter3
